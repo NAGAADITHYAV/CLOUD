55df61010b47bd7600ce0e5c6b5009a4ac45ec71
NAGA ADITHYA <nagaadithyavuppalapati@gmail.com>
2025-03-24 01:36:40 -0700

    commit

--- controller.py
+++ controller.py
@@ -1,23 +1,21 @@
 import boto3
 import os
+from time import sleep
 # ---------- Configuration ----------
 ASU_ID = '1230415071'
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 REQ_QUEUE = 'https://sqs.us-east-1.amazonaws.com/340752817731/1230415071-req-queue'
 RESP_QUEUE = 'https://sqs.us-east-1.amazonaws.com/340752817731/1230415071-resp-queue'
 MAX_INSTANCES = 15
-AMI_ID = 'ami-03f5b9cd81d07aaef'
+AMI_ID = 'ami-0b6fbc8df0a65b120'
 # ---------- AWS Setup ----------
 session = boto3.Session(
-    aws_access_key_id = AWS_ACCESS_KEY_ID,
-    aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
     region_name = "us-east-1"
 )
 sqs = session.client('sqs')
-ec2 = session.resource('ec2')
-input_bucket = f'{ASU_ID}-in-bucket'
-
+ec2 = boto3.resource(
+    'ec2',
+    region_name="us-east-1"
+)
 
 def get_queue_length():
     attributes = sqs.get_queue_attributes(
@@ -26,52 +24,53 @@ def get_queue_length():
     )
     return int(attributes['Attributes'].get('ApproximateNumberOfMessages', '0'))
 
-def autoscale_instances(queue_count):
-    target_instance_count = min(MAX_INSTANCES, queue_count)
-    if queue_count == 0:
-        target_instance_count = 0
-    existing_instances = list(ec2.instances.filter(
+def fetch_instances():
+    return list(ec2.instances.filter(
         Filters=[
             {'Name': 'tag:AppTier', 'Values': ['true']},
             {'Name': 'instance-state-name', 'Values': ['running', 'pending']}
-        ]
-    ))
-    active_instance_count = len(existing_instances)
-    if active_instance_count < target_instance_count:
-        instances_needed = target_instance_count - active_instance_count
-        for i in range(instances_needed):
-            instance_number = active_instance_count + i + 1
-            ec2.create_instances(
-                ImageId=AMI_ID,
-                MinCount=1,
-                MaxCount=1,
-                InstanceType='t2.micro',
-                KeyName='Project1',
-                IamInstanceProfile={'Name': 'IAM-DEV-ADI'},
-                TagSpecifications=[{
-                    'ResourceType': 'instance',
-                    'Tags': [
-                        {'Key': 'Name', 'Value': f'app-tier-instance-{instance_number}'},
-                        {'Key': 'AppTier', 'Value': 'true'}
-                    ]
-                }],
-                UserData="""#!/bin/bash
-                cd ~
-                source myenv/bin/activate
-                export AWS_SECRET_ACCESS_KEY={AWS_SECRET_ACCESS_KEY}
-                export AWS_ACCESS_KEY_ID={AWS_ACCESS_KEY_ID}
-                cd CLOUD/
-                nohup python3 backend.py &
-                """
-            )
-            print(f"Started app-tier-instance-{instance_number}.")
-    elif active_instance_count > target_instance_count:
-        instances_to_stop = [inst.id for inst in existing_instances[target_instance_count:]]
-        ec2.instances.filter(InstanceIds=instances_to_stop).stop()
-        print(f"Stopped {len(instances_to_stop)} excess App Tier instances.")
+            ]))
 
-    
+def create_instance(instance_number):
+    print("launching instance:",instance_number)
+    return ec2.create_instances(
+        ImageId=AMI_ID,
+        MinCount=1,
+        MaxCount=1,
+        InstanceType='t2.micro',
+        KeyName='project1',
+        TagSpecifications=[{
+            'ResourceType': 'instance',
+            'Tags': [
+                {'Key': 'Name', 'Value': f'app-tier-instance-{instance_number}'},
+                {'Key': 'AppTier', 'Value': 'true'}
+            ]
+        }],
+        UserData="""#!/bin/bash
+                cd /home/ubuntu/
+                source /home/ubuntu/myenv/bin/activate
+                cd /home/ubuntu/CLOUD
+                nohup python3 backend.py > backend.log 2>&1 &
+                """
+    )
+
+def autoscale_instances(queue_count):
+    required_count = min(MAX_INSTANCES, queue_count)
+    if queue_count==0 and required_count>0:
+        required_count=0
+    current_instances = fetch_instances()
+    if len(current_instances) < required_count:
+        more_required = required_count - len(current_instances)
+        for i in range(more_required):
+            create_instance(len(current_instances) + 1 + i)
+            print("Launched insrtance sucessfully")
+    elif len(current_instances) > required_count:
+        instances_to_terminate = []
+        for inst in current_instances[required_count:]:
+            instances_to_terminate.append(inst.id)
+        ec2.instances.filter(InstanceIds=instances_to_terminate).terminate()
 
 if __name__ == "__main__":
     while True:
         autoscale_instances(get_queue_length())
+        sleep(1)
--- server.py
+++ server.py
@@ -12,8 +12,6 @@ from starlette.middleware.trustedhost import TrustedHostMiddleware
 ASU_ID = "1230415071"
 S3_BUCKET_NAME = f"{ASU_ID}-in-bucket"
 SIMPLEDB_DOMAIN = f"{ASU_ID}-simpleDB"
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 PORT = 8000
 RESULTS = {}
 QWAIT = {}
@@ -23,8 +21,6 @@ RESP_QUEUE = 'https://sqs.us-east-1.amazonaws.com/340752817731/1230415071-resp-q
 
 # ---------- AWS Setup ----------
 session = boto3.Session(
-    aws_access_key_id = AWS_ACCESS_KEY_ID,
-    aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
     region_name = "us-east-1"
 )
 s3 = session.client('s3')
